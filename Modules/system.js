7bbc34e81d2fa08726a93c1d3b2d9ebb7b019c6f
NSALAA9 <alaansairat99@gmail.com>
2023-07-10 11:20:18 +0000

    lab 11

---
+++ Modules/system.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const eventsPool = require('../eventspool');
+require('./pilot');
+require('./manager');
+
+// Listen for the events and print the details
+eventsPool.on('new-flight', (flightDetails) => {
+ //console.log('Manager: new flight with ID', flightDetails.details.flightID, 'has been scheduled');
+  console.log('Flight:', flightDetails);
+});
+
+eventsPool.on('took-off', (flightDetails) => {
+  //console.log('Pilot: flight with ID', flightDetails.flightID, 'took-off');
+  console.log('Flight:', flightDetails);
+});
+
+eventsPool.on('arrived', (flightDetails) => {
+  //console.log('Pilot: flight with ID', flightDetails.flightID, 'has arrived');
+  console.log('Flight:', flightDetails);
+
+  console.log('Manager: we are greatly thankful for the amazing flight,', flightDetails.details.pilot);
+});
